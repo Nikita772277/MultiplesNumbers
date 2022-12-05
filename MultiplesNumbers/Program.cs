195cdcfb638627dfa2f1f64900f9511308650d32
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-12-05 11:42:37 +0300

    Добавьте файлы проекта.

---
+++ MultiplesNumbers/Program.cs
@@ -0,0 +1,7 @@
+﻿using MultiplesNumbers;
+
+Console.WriteLine($"Введите число");
+string enternumber=Console.ReadLine();
+bool checknumber=int.TryParse(enternumber, out int resultnumber);
+Work work = new();
+work.SearchMultiples(resultnumber);
---
+++ MultiplesNumbers/Work.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesNumbers
+{
+    public class Work
+    {
+        public void SearchMultiples(int number)
+        {
+            int summ = 0;
+            for (int i = 0; i < number; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    continue;
+                }
+                else if (i % 3 == 0 || i % 5 == 0)
+                {
+                    summ += i;
+                }
+            }
+            Console.WriteLine($"Сумма чисел кратных 3 и 5 не слаживается в сумму");
+            Console.WriteLine($"Сумма чисел кратных 3 или 5: {summ}");
+        }
+
+    }
+}
