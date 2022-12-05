5b8c20d77a1077878d74358d6848a1e6ec039cc9
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-12-05 11:54:01 +0300

    132

--- MultiplesNumbers/Work.cs
+++ MultiplesNumbers/Work.cs
@@ -13,16 +13,11 @@ namespace MultiplesNumbers
             int summ = 0;
             for (int i = 0; i < number; i++)
             {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    continue;
-                }
-                else if (i % 3 == 0 || i % 5 == 0)
+                if (i % 3 == 0 || i % 5 == 0)
                 {
                     summ += i;
                 }
             }
-            Console.WriteLine($"Сумма чисел кратных 3 и 5 не слаживается в сумму");
             Console.WriteLine($"Сумма чисел кратных 3 или 5: {summ}");
         }
 
